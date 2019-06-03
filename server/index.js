b7081778c8782885101116bc96af78e0ee4f761c
Camron Stuart <Samlennder@gmail.com>
2019-06-03 15:00:53 -0400

    update proxy

--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
+const express = require('express');
+const port = 3000;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const app = express();
 
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/../public'))
-app.use(bodyParser.json())
+app.use(express.static(__dirname + '/../public'));
+
+app.get('/api/listings/:id/reservations', (req, res) => {
+  const { id } = req.params;
+  axios.get(`http://localhost:3002/api/listings/${id}/reservations`)
+    .then(response=> res.send(response.data))
+    .catch(err => {
+      console.log('ERROR: ', err);
+      res.json(err)
+    });
+});
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
